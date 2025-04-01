5f539d27da5f47d0c5b900df7ce5d3f25fdd1576
polinazharova <zharowa.polya@yandex.ru>
2025-04-01 16:05:20 +0300

    change job name & change logItem test

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "./styles/index.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
